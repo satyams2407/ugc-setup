d4e343da605e17ff0bbde12c8707303d37f8d348
Satyam Singh <satyams2407@gmail.com>
2026-04-08 18:25:05 +0530

    first commit

---
+++ server/configs/instrument.mjs
@@ -0,0 +1,6 @@
+import * as Sentry from "@sentry/node";
+
+Sentry.init({
+    dsn: "https://bbe66e23322d9ac9fb7ca144ae3445f1@o4511161607192577.ingest.us.sentry.io/4511184486137856",
+    sendDefaultPii: true,
+});
